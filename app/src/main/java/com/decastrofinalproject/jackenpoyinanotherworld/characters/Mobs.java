f828ad0df273b6bb9e6d233eea03f2c5786a59ac
Hase0278 <betterhalf278@gmail.com>
2022-01-19 18:14:36 +0800

    Round Initial

--- app/src/main/java/com/decastrofinalproject/jackenpoyinanotherworld/characters/Mobs.java
+++ app/src/main/java/com/decastrofinalproject/jackenpoyinanotherworld/characters/Mobs.java
@@ -4,8 +4,8 @@ import com.decastrofinalproject.jackenpoyinanotherworld.R;
 
 public class Mobs extends Characters{
     public Mobs(String side, int round){
-        this.characterImg = side.equals("humans")? R.drawable.demonmobs:R.drawable.humanmobs;
-        this.charName = side.equals("humans")? "Enemy soldier <num>(Demon)":"Enemy soldier <num>(Human)";
+        this.characterImg = side.equals("human")? R.drawable.demonmobs:R.drawable.humanmobs;
+        this.charName = side.equals("human")? "Enemy soldier <num>(Demon)":"Enemy soldier <num>(Human)";
         setDmg(5);
         setMultiplier(round);
         setHp(50);
