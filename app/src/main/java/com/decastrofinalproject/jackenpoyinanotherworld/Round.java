e2669ac2c2107bc76784f6cfb66c6370989d08b1
Hase0278 <betterhalf278@gmail.com>
2022-01-23 15:11:46 +0800

    You win dialogue

--- app/src/main/java/com/decastrofinalproject/jackenpoyinanotherworld/Round.java
+++ app/src/main/java/com/decastrofinalproject/jackenpoyinanotherworld/Round.java
@@ -6,12 +6,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
